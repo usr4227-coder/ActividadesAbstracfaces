e0e73d646fcf9c23f392e59827d8fe46f42c667e
Nick <usr4227@salesianos-lacuesta.net>
2026-04-24 18:06:47 +0100

    feat: Creacion de clase Bidimensional

---
+++ src/net/salesianos/figuras/CalculoBidimensional.java
@@ -0,0 +1,12 @@
+package net.salesianos.figuras;
+
+/*
+ * Interfaz que define el cálculo geométrico bidimensional.
+ * Obliga a implementar los métodos para calcular área y perímetro.
+ */
+public interface CalculoBidimensional {
+
+    double calcularArea();
+
+    double calcularPerimetro();
+}
